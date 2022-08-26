adcda01f30848e5ebcf96fea73c8b665a12ad055
Nicholas Sneed <31823460+stlbearboy@users.noreply.github.com>
2022-08-25 19:41:39 -0500

    Change list size to 5

--- docs/plots.js
+++ docs/plots.js
@@ -10,7 +10,7 @@ d3.json("data/data.json").then((importedData) => {
   });
 
   // Slice the first 10 objects for plotting
-  data = data.slice(0, 10);
+  data = data.slice(0, 5);
 
   // Reverse the array due to Plotly's defaults
   data = data.reverse();
